b9f2575294b4139190c2b2f28264fcdc11930f01
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-19 21:34:00 +0300

    slug model

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -2,11 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\SlugOptions;
 
+
+#[Guarded(['id'])]
 class Business extends Model
 {
     /** @use HasFactory<\Database\Factories\BusinessFactory> */
     use HasFactory;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
