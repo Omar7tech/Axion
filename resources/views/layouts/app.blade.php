00b54a18cd7ec96528e4caf63d79c96c8858aadc
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-19 21:14:14 +0300

    filament

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ 'AXION - ' . ($title ?? config('app.name')) }}</title>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Axion" />
+    <link rel="manifest" href="/site.webmanifest" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
 
-        @livewireStyles
-    </head>
-    <body>
-        {{ $slot }}
+<body>
+    {{ $slot }}
 
-        @livewireScripts
-    </body>
+    @livewireScripts
+</body>
+
 </html>
