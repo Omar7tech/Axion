2f156c89dcdf8e92b07d3c084508d760ec82b544
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-23 00:57:56 +0300

    sdfsdf

--- resources/views/pages/investment/⚡index/index.php
+++ resources/views/pages/investment/⚡index/index.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Models\Investment;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 new class extends Component
 {
-    //
+    use WithPagination;
+
+    public int $perPage = 9;
+
+    #[Computed]
+    public function investments()
+    {
+        return Investment::active()
+            ->latest('created_at')
+            ->paginate($this->perPage);
+    }
+
+    #[Computed]
+    public function totalCount()
+    {
+        return Investment::active()->count();
+    }
 };
--- resources/views/pages/investment/⚡show/show.php
+++ resources/views/pages/investment/⚡show/show.php
@@ -1,8 +1,35 @@
 <?php
 
 use App\Models\Investment;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 new class extends Component {
     public Investment $investment;
+
+    #[Computed]
+    public function coverImage()
+    {
+        return $this->investment->hasMedia('images')
+            ? $this->investment->getFirstMediaUrl('images', 'webp')
+            : asset('covers/investment-placeholder.png');
+    }
+
+    #[Computed]
+    public function galleryImages()
+    {
+        return $this->investment->getMedia('images')->skip(1);
+    }
+
+    #[Computed]
+    public function hasCoverImage()
+    {
+        return $this->investment->hasMedia('images');
+    }
+
+    #[Computed]
+    public function hasGallery()
+    {
+        return $this->investment->getMedia('images')->count() > 1;
+    }
 };
