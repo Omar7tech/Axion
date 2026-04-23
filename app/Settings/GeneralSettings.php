76e3b29eddfb0c60170a28a83e1e22110ec0f6d7
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-23 20:28:33 +0300

    general settings

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -7,6 +7,18 @@ use Spatie\LaravelSettings\Settings;
 class GeneralSettings extends Settings
 {
 
+     public array $social_media = [];
+
+    public ?string $about_us = 'Axion About Us';
+
+    public array $contact_emails = [];
+
+    public array $contact_phones = [];
+
+    public bool $whatsapp_badge_visible = false;
+
+    public ?string $whatsapp_phone_number = '';
+
     public static function group(): string
     {
         return 'general';
