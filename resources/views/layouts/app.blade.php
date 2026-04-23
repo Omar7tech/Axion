b1cc09f54c5d91967aa0c9c083c3779b53fad311
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-23 16:04:23 +0300

    edit layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="Axion" />
     <link rel="manifest" href="/site.webmanifest" />
+   <!--  <link rel="stylesheet" href="https://unpkg.com/kursor/dist/kursor.css"> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
@@ -29,5 +30,13 @@
     @livewireScripts
     <!-- <script src="https://cdn.jsdelivr.net/gh/SH20RAJ/ScrollProgressJS@main/ScrollProgress.js"></script> -->
 </body>
+<!-- <script src="https://unpkg.com/kursor"></script>
+<script>
+    new kursor({
+        removeDefaultCursor: true,
+        type: 1,
+        color: '#ffffff'
+    })
+</script> -->
 
 </html>
