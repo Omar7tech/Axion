ff6e1da2b00f41ced40f4e651cf980a59f07628d
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-27 21:34:26 +0300

    Brain trust

---
+++ database/factories/BrainTrustFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BrainTrust;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<BrainTrust>
+ */
+class BrainTrustFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
