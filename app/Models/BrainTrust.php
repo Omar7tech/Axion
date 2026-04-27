e316204f53809232076acafc8839815ff2681548
Omar Abi Farraj <omar.7tech@gmail.com>
2026-04-27 21:39:11 +0300

    migration and model BrainTrust

--- app/Models/BrainTrust.php
+++ app/Models/BrainTrust.php
@@ -2,11 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Guarded;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\Sluggable\HasSlug;
 
+
+#[Guarded(['id'])]
 class BrainTrust extends Model
 {
     /** @use HasFactory<\Database\Factories\BrainTrustFactory> */
-    use HasFactory;
+    use HasFactory , InteractsWithMedia;
+
+     public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('webp')
+            ->format('webp')
+            ->quality(20)
+            ->nonQueued()
+            ->performOnCollections('cover');
+    }
+
+    #[Scope]
+    protected function active(Builder $query): void
+    {
+        $query->where('is_active', true);
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('sort', function (Builder $builder) {
+            $builder->orderBy('sort', 'asc');
+        });
+        static::addGlobalScope('active', function (Builder $builder) {
+            $builder->where('is_active', true);
+        });
+    }
 }
--- database/migrations/2026_04_27_183414_create_brain_trusts_table.php
+++ database/migrations/2026_04_27_183414_create_brain_trusts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('brain_trusts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('sort')->default(0);
+            $table->boolean('is_active')->default(true);
+            $table->string('role');
             $table->timestamps();
         });
     }
